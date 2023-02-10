39c440ce33ed8178e6df917e3aef93705132a64b
phaneendra <phaneendrapilli777@gmail.com>
2023-02-10 20:22:35 +0530

    event page modifiedfirst commit

--- src/pages/routing/Events.js
+++ src/pages/routing/Events.js
@@ -1,150 +1,16 @@
+import { useEffect, useRef, useState } from "react";
+import Heuristic from "./events/Heuristic";
+import TechConnect from "./events/TechConnect";
+
 const Events = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center bg-home-bg ">
-        <img
-          src="/poster.png"
-          className=" h-[80%] sm:h-full w-[100%] sm:w-[90%] "
-        />
-        <div className="mt-14 flex  text-white flex-col">
-          <div className="px-14 w-screen h-96 flex justify-between mb-14 overflow-auto">
-            <div className="w-1/2">
-              <h1 className=" sm:text-3xl pb-12">
-                <a
-                  href="https://docs.google.com/forms/d/e/1FAIpQLSeug5dFqeaejA6oxFBu_7sENCT13m3DuV80vrCDBP-CeuNiSw/viewform?usp=sf_link"
-                  className="no-underline"
-                >
-                  Tech connect
-                </a>
-              </h1>
-              <div className="h-52">
-                Rules:<br></br>
-                1.Only individual participation is allowed to participate in the
-                event.<br></br>
-                2.Participants must have "Unstop" account (details will be
-                provided in prior).<br></br>
-                Participant should participate in the given allotted time.
-                <br></br>
-                Guidelines:<br></br>
-                1.Participants will be shortlisted in each round depending on
-                score secured. 2.In case of time breaker, we will
-                consider best time.
-              </div>
-            </div>
-            <div className="w-1/2 h-72 flex flex-col  justify-start gap-10 items-center">
-              <img src="/technook.jpg" className="w-64" />
-              <div className="w-36 bg-blue-800 text-center">
-                <a
-                  className="no-underline"
-                  href="https://docs.google.com/forms/d/e/1FAIpQLSeug5dFqeaejA6oxFBu_7sENCT13m3DuV80vrCDBP-CeuNiSw/viewform?usp=sf_link"
-                >
-                  register
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="px-14 w-screen h-96 flex justify-between mb-14 overflow-auto">
-            <div className="w-1/2">
-              <h1 className=" sm:text-3xl pb-12">
-                <a
-                  className="no-underline"
-                  href="
-                https://docs.google.com/forms/d/e/1FAIpQLSfhaVwSLqJfhrGimmydDMVUXdXmqrrYQKPGPoU5GYV4RxDOyg/viewform?usp=sf_link"
-                >
-                  Tricky Zone
-                </a>
-              </h1>
-              <div className="h-52">
-                Round-1:<br></br> The participation should be individual.
-                <br></br>  The participant must have to fill the answer that
-                match with the word in the puzzle.<br></br> Round-2:<br></br>
-                The students filtered in Round 1 only will be allowed to
-                Round-2. <br></br> The participant will be given a code and
-                they have to rewrite the code in the optimized way
-                (better efficiency).
-              </div>
-            </div>
-            <div className="w-1/2 h-72 flex flex-col  justify-start gap-10 items-center">
-              <img src="/trickyZone.jpg" className="w-64" />
-              <div className="w-36 bg-blue-800 text-center">
-                <a
-                  className="no-underline"
-                  href="https://docs.google.com/forms/d/e/1FAIpQLSfhaVwSLqJfhrGimmydDMVUXdXmqrrYQKPGPoU5GYV4RxDOyg/viewform?usp=sf_link"
-                >
-                  register
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="px-14 w-screen h-96 flex  justify-between mb-14 overflow-auto">
-            <div className="w-1/2">
-              <h1 className=" sm:text-3xl pb-12">
-                <a
-                  className="no-underline"
-                  href="https://docs.google.com/forms/d/e/1FAIpQLSfcZROXVGluTj_ZJwlTRlp6e4lpTGNjNW-4WNVJ3zjN4Gv8hw/viewform?usp=sf_link"
-                >
-                  Adventure Quest
-                </a>
-              </h1>
-              <div className="h-52">
-                Number of participants restricted to 1 member only. Guidelines:-
-                <br></br>
-                1)Every registered student can access the link only once.
-                <br></br> 2) Participants should complete the challenges within
-                the time allotted, to enter into next level. <br></br> 3) The
-                participant who clears all the challenges as early as possible
-                will be awarded the first position and remaining as follows.
-                <br></br> 4)Only the person who registered will be able to take
-                the quest from the registered email id
-              </div>
-            </div>
-            <div className="w-1/2 h-72 flex flex-col  justify-start gap-10 items-center">
-              <img src="/adventure.jpg" className="w-64" />
-              <div className="w-36 bg-blue-800 text-center">
-                <a
-                  className="no-underline"
-                  href="https://docs.google.com/forms/d/e/1FAIpQLSfcZROXVGluTj_ZJwlTRlp6e4lpTGNjNW-4WNVJ3zjN4Gv8hw/viewform?usp=sf_link"
-                >
-                  register
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className="px-14 w-screen h-96 flex  justify-between overflow-auto">
-            <div className="w-1/2">
-              <h1 className=" sm:text-3xl pb-12">
-                <a
-                  className="no-underline"
-                  href="https://docs.google.com/forms/d/e/1FAIpQLSfCjCd7DEb6YU0RpdcKYA2PpXVMWKq8lnbpH1KL37xQV68e2A/viewform"
-                >
-                  Heuristic Flick
-                </a>
-              </h1>
-              <div className="h-52">
-                Number of participants can be 3 - 6 members .<br></br> Video
-                limit should be 4-7 minutes.<br></br> Guidelines:<br></br> 1.The
-                participant must choose from the topics given .<br></br> 2.The
-                video presentations can be done in any of the three ways - short
-                videos or animations or even slideshows with transitions with
-                voiceover (preferred not mandatory).<br></br> 3.Strictly
-                restrict any kind of vulgarity in the film.<br></br>{" "}
-                4.Evaluation will be done based on the judgement
-                during the event .
-              </div>
-            </div>
-            <div className="w-1/2 h-72 flex flex-col  justify-start gap-10 items-center">
-              <img src="/heuristicFlick.jpg" className="w-64" />
-              <a
-                className="no-underline"
-                href="https://docs.google.com/forms/d/e/1FAIpQLSfCjCd7DEb6YU0RpdcKYA2PpXVMWKq8lnbpH1KL37xQV68e2A/viewform"
-              >
-                <div className="w-36 bg-blue-800 text-center">register</div>
-              </a>
-            </div>
-          </div>
+      <div className="w-screen flex flex-col justify-center items-center bg-[#14141c]  text-white ">
+        {/* events parent body div */}
+        <div className=" w-[95%] sm:w-[90%] flex flex-col">
+          {/* main grid  */}
+          <Heuristic />
+          <TechConnect />
         </div>
       </div>
     </>
---
+++ src/pages/routing/events/TechConnect.js
@@ -0,0 +1,90 @@
+import Image from "next/image";
+
+import { useInView } from "react-intersection-observer";
+
+const TechConnect = () => {
+  const { ref, inView, entry } = useInView();
+  console.log(inView);
+  console.log(entry);
+
+  const eventRules = [
+    "some random text to display",
+    "some random text to display",
+    "some random text to display",
+    "some random text to display",
+  ];
+
+  return (
+    <div className="w-full md:flex-col md:items-center px-10 ">
+      <div className="md:flex md:justify-between">
+        <div className="text-[#64239f] text-4xl">TechConnect</div>
+        <div className=" m-10 w-5/6 sm:w-1/2 border-2 border-purple-700  sm:h-[70vh] flex flex-col opacity-70">
+          {/* card with image div */}
+          <div>
+            <Image
+              src="/eventpics/huristic.jpeg"
+              width={400}
+              height={100}
+              className="w-full h-56"
+            />
+          </div>
+          <div className=" h-full w-full flex flex-col justify-evenly p-5">
+            {/* bottom card div */}
+
+            <div className="my-3">
+              Only individual participation is allowed to participate in the
+              event.Participants must have "Unstop" account (details will be
+              provided in prior). Participant should participate in the given
+              allotted time.
+            </div>
+            <div className="flex justify-around">
+              <button className="border-2 p-2 w-36 border-purple-700 hover:bg-[#8729d9] ">
+                Register
+              </button>
+              <button className="border-2 p-2 w-36 border-purple-700 hover:bg-[#8729d9]  ease-in">
+                View more
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full flex flex-col">
+        <div className="p-5 flex flex-col">
+          <div className="text-violet-700 text-3xl">Event Rules</div>
+          <ul
+            className={`my-10 md:flex md:justify-evenly ${
+              inView
+                ? "transition duration-1000 ease-out translate-x-0"
+                : "translate-x-[-100%] blur-sm transition-all duration-1000 opacity-0"
+            } gap-6`}
+            ref={ref}
+          >
+            {eventRules.map((rule) => {
+              return (
+                <li className="p-2 h-32 flex  justify-center items-center border-2 border-purple-700">
+                  <span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="w-6 h-6"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </span>
+                  {rule}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TechConnect;
