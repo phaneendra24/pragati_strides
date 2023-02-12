0245067175410cc9bae7a7c6c20110e713b0280a
phaneendra <phaneendrapilli777@gmail.com>
2023-02-12 14:32:04 +0530

    updated sizes

--- src/pages/home/Welcome.js
+++ src/pages/home/Welcome.js
@@ -3,7 +3,7 @@ import Typewriter from "typewriter-effect";
 const Welcome = () => {
   return (
     <div className="welcome-greet ">
-      <div className="text-3xl text-teal-800 h-32 w-full ">
+      <div className="text-3xl text-teal-800 h-24 bg-red-900 w-full ">
         <Typewriter
           options={{
             strings: [
