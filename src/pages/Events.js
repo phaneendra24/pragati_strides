c2e57e091150ca8e7c0a2553b61a91513f2a1fc7
phaneendra <phaneendrapilli777@gmail.com>
2023-02-13 06:56:07 +0530

    minor size changes

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -8,7 +8,7 @@ const Events = () => {
   return (
     <div className="w-screen flex flex-col justify-center items-center bg-[#14141c]  text-white ">
       {/* events parent body div */}
-      <div className=" w-[95%] sm:w-[90%] flex flex-col">
+      <div className="sm:w-[90%] flex flex-col">
         {/* main grid  */}
         <Heuristic />
         <TechConnect />
