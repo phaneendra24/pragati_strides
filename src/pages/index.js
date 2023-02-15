fd08a5716a70210b29a456c2a10ab8f66c8aa550
phaneendra <phaneendrapilli777@gmail.com>
2023-02-15 12:03:48 +0530

    margin fixing

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,13 +27,13 @@ export default function Home() {
                 all about how people choose to use it.
               </div>
               <Welcome />
-              <div>
+              <div className="">
                 <a href="https://docs.google.com/forms/d/e/1FAIpQLSdBfdmESh0BrMTqPeG42sH16n7n8UoR6ruFTABKetuzvj3FIQ/viewform?usp=sf_link">
                   <motion.button
                     whileHover={{ scale: 1.1 }}
                     whileTap={{ scale: 0.9 }}
                     type="button"
-                    className="w-48 mt-10 text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center  mb-2"
+                    className="w-48 text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center  mb-2"
                   >
                     Register Now
                   </motion.button>
